b03a41f41df3565684abacd8db85dad439e8df3e
ChrisChamalet <60783435+ChristopheChamalet@users.noreply.github.com>
2021-12-07 14:26:43 +0100

    Delete Pacman directory

--- Pacman/Form1.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form1
-    Private Sub PictureBox7_Click(sender As Object, e As EventArgs) Handles PictureBox7.Click
-
-    End Sub
-
-    Private Sub PictureBox16_Click(sender As Object, e As EventArgs) Handles redGhost.Click
-
-    End Sub
-End Class
