a58076b9c5567c66c3c4a7f368fc7de1ce1f8780
Abder100 <ayourmeche@gmail.com>
2021-12-07 14:10:36 +0100

    la map du Pacman

---
+++ Pacman/Form1.Designer.vb
@@ -0,0 +1,387 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox10 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox11 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox12 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox13 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox14 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox15 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox17 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox18 = New System.Windows.Forms.PictureBox()
+        Me.redGhost = New System.Windows.Forms.PictureBox()
+        Me.yellowGhost = New System.Windows.Forms.PictureBox()
+        Me.pinkGhost = New System.Windows.Forms.PictureBox()
+        Me.PictureBox16 = New System.Windows.Forms.PictureBox()
+        Me.Pacman = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox13, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox14, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox15, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox17, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox18, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.redGhost, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.yellowGhost, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pinkGhost, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox16, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pacman, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.Window
+        Me.Label1.Location = New System.Drawing.Point(700, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(98, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Score : 0"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Blue
+        Me.PictureBox1.Location = New System.Drawing.Point(-1, 57)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(1373, 20)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Tag = "wall"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Blue
+        Me.PictureBox2.Location = New System.Drawing.Point(-1, 676)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(1373, 20)
+        Me.PictureBox2.TabIndex = 2
+        Me.PictureBox2.TabStop = False
+        Me.PictureBox2.Tag = "wall"
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.Blue
+        Me.PictureBox3.Location = New System.Drawing.Point(-1, 72)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(20, 605)
+        Me.PictureBox3.TabIndex = 3
+        Me.PictureBox3.TabStop = False
+        Me.PictureBox3.Tag = "wall"
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.Color.Blue
+        Me.PictureBox4.Location = New System.Drawing.Point(1351, 72)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(20, 605)
+        Me.PictureBox4.TabIndex = 4
+        Me.PictureBox4.TabStop = False
+        Me.PictureBox4.Tag = "wall"
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BackColor = System.Drawing.Color.Blue
+        Me.PictureBox5.Location = New System.Drawing.Point(333, 499)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(32, 178)
+        Me.PictureBox5.TabIndex = 5
+        Me.PictureBox5.TabStop = False
+        Me.PictureBox5.Tag = "wall"
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.BackColor = System.Drawing.Color.Blue
+        Me.PictureBox6.Location = New System.Drawing.Point(1194, 72)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(19, 68)
+        Me.PictureBox6.TabIndex = 6
+        Me.PictureBox6.TabStop = False
+        Me.PictureBox6.Tag = "wall"
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.BackColor = System.Drawing.Color.Blue
+        Me.PictureBox7.Location = New System.Drawing.Point(1162, 202)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(112, 20)
+        Me.PictureBox7.TabIndex = 7
+        Me.PictureBox7.TabStop = False
+        Me.PictureBox7.Tag = "wall"
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.BackColor = System.Drawing.Color.Blue
+        Me.PictureBox8.Location = New System.Drawing.Point(1270, 440)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(19, 177)
+        Me.PictureBox8.TabIndex = 8
+        Me.PictureBox8.TabStop = False
+        Me.PictureBox8.Tag = "wall"
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.BackColor = System.Drawing.Color.Blue
+        Me.PictureBox9.Location = New System.Drawing.Point(1270, 135)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(20, 137)
+        Me.PictureBox9.TabIndex = 9
+        Me.PictureBox9.TabStop = False
+        Me.PictureBox9.Tag = "wall"
+        '
+        'PictureBox10
+        '
+        Me.PictureBox10.BackColor = System.Drawing.Color.Blue
+        Me.PictureBox10.Location = New System.Drawing.Point(1162, 524)
+        Me.PictureBox10.Name = "PictureBox10"
+        Me.PictureBox10.Size = New System.Drawing.Size(112, 20)
+        Me.PictureBox10.TabIndex = 10
+        Me.PictureBox10.TabStop = False
+        Me.PictureBox10.Tag = "wall"
+        '
+        'PictureBox11
+        '
+        Me.PictureBox11.BackColor = System.Drawing.Color.Blue
+        Me.PictureBox11.Location = New System.Drawing.Point(1194, 283)
+        Me.PictureBox11.Name = "PictureBox11"
+        Me.PictureBox11.Size = New System.Drawing.Size(19, 177)
+        Me.PictureBox11.TabIndex = 11
+        Me.PictureBox11.TabStop = False
+        Me.PictureBox11.Tag = "wall"
+        '
+        'PictureBox12
+        '
+        Me.PictureBox12.BackColor = System.Drawing.Color.Blue
+        Me.PictureBox12.Location = New System.Drawing.Point(1212, 350)
+        Me.PictureBox12.Name = "PictureBox12"
+        Me.PictureBox12.Size = New System.Drawing.Size(78, 20)
+        Me.PictureBox12.TabIndex = 12
+        Me.PictureBox12.TabStop = False
+        Me.PictureBox12.Tag = "wall"
+        '
+        'PictureBox13
+        '
+        Me.PictureBox13.BackColor = System.Drawing.Color.Blue
+        Me.PictureBox13.Location = New System.Drawing.Point(779, 283)
+        Me.PictureBox13.Name = "PictureBox13"
+        Me.PictureBox13.Size = New System.Drawing.Size(19, 177)
+        Me.PictureBox13.TabIndex = 13
+        Me.PictureBox13.TabStop = False
+        Me.PictureBox13.Tag = "wall"
+        '
+        'PictureBox14
+        '
+        Me.PictureBox14.BackColor = System.Drawing.Color.Blue
+        Me.PictureBox14.Location = New System.Drawing.Point(671, 440)
+        Me.PictureBox14.Name = "PictureBox14"
+        Me.PictureBox14.Size = New System.Drawing.Size(112, 20)
+        Me.PictureBox14.TabIndex = 14
+        Me.PictureBox14.TabStop = False
+        Me.PictureBox14.Tag = "wall"
+        '
+        'PictureBox15
+        '
+        Me.PictureBox15.BackColor = System.Drawing.Color.Blue
+        Me.PictureBox15.Location = New System.Drawing.Point(671, 283)
+        Me.PictureBox15.Name = "PictureBox15"
+        Me.PictureBox15.Size = New System.Drawing.Size(112, 20)
+        Me.PictureBox15.TabIndex = 15
+        Me.PictureBox15.TabStop = False
+        Me.PictureBox15.Tag = "wall"
+        '
+        'PictureBox17
+        '
+        Me.PictureBox17.BackColor = System.Drawing.Color.Blue
+        Me.PictureBox17.Location = New System.Drawing.Point(655, 411)
+        Me.PictureBox17.Name = "PictureBox17"
+        Me.PictureBox17.Size = New System.Drawing.Size(19, 49)
+        Me.PictureBox17.TabIndex = 17
+        Me.PictureBox17.TabStop = False
+        Me.PictureBox17.Tag = "wall"
+        '
+        'PictureBox18
+        '
+        Me.PictureBox18.BackColor = System.Drawing.Color.Blue
+        Me.PictureBox18.Location = New System.Drawing.Point(655, 283)
+        Me.PictureBox18.Name = "PictureBox18"
+        Me.PictureBox18.Size = New System.Drawing.Size(19, 49)
+        Me.PictureBox18.TabIndex = 18
+        Me.PictureBox18.TabStop = False
+        Me.PictureBox18.Tag = "wall"
+        '
+        'redGhost
+        '
+        Me.redGhost.Image = Global.Pacman.My.Resources.Resources.red_guy
+        Me.redGhost.Location = New System.Drawing.Point(743, 325)
+        Me.redGhost.Name = "redGhost"
+        Me.redGhost.Size = New System.Drawing.Size(30, 45)
+        Me.redGhost.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.redGhost.TabIndex = 19
+        Me.redGhost.TabStop = False
+        Me.redGhost.Tag = "ghost"
+        '
+        'yellowGhost
+        '
+        Me.yellowGhost.Image = Global.Pacman.My.Resources.Resources.yellow_guy
+        Me.yellowGhost.Location = New System.Drawing.Point(691, 345)
+        Me.yellowGhost.Name = "yellowGhost"
+        Me.yellowGhost.Size = New System.Drawing.Size(30, 45)
+        Me.yellowGhost.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.yellowGhost.TabIndex = 20
+        Me.yellowGhost.TabStop = False
+        Me.yellowGhost.Tag = "ghost"
+        '
+        'pinkGhost
+        '
+        Me.pinkGhost.Image = Global.Pacman.My.Resources.Resources.pink_guy
+        Me.pinkGhost.Location = New System.Drawing.Point(743, 389)
+        Me.pinkGhost.Name = "pinkGhost"
+        Me.pinkGhost.Size = New System.Drawing.Size(30, 45)
+        Me.pinkGhost.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pinkGhost.TabIndex = 21
+        Me.pinkGhost.TabStop = False
+        Me.pinkGhost.Tag = "ghost"
+        '
+        'PictureBox16
+        '
+        Me.PictureBox16.BackColor = System.Drawing.Color.Blue
+        Me.PictureBox16.Location = New System.Drawing.Point(1194, 609)
+        Me.PictureBox16.Name = "PictureBox16"
+        Me.PictureBox16.Size = New System.Drawing.Size(19, 68)
+        Me.PictureBox16.TabIndex = 22
+        Me.PictureBox16.TabStop = False
+        Me.PictureBox16.Tag = "wall"
+        '
+        'Pacman
+        '
+        Me.Pacman.Image = Global.Pacman.My.Resources.Resources.right
+        Me.Pacman.Location = New System.Drawing.Point(1138, 345)
+        Me.Pacman.Name = "Pacman"
+        Me.Pacman.Size = New System.Drawing.Size(50, 50)
+        Me.Pacman.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.Pacman.TabIndex = 23
+        Me.Pacman.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.MenuText
+        Me.ClientSize = New System.Drawing.Size(1384, 761)
+        Me.Controls.Add(Me.Pacman)
+        Me.Controls.Add(Me.PictureBox16)
+        Me.Controls.Add(Me.pinkGhost)
+        Me.Controls.Add(Me.yellowGhost)
+        Me.Controls.Add(Me.redGhost)
+        Me.Controls.Add(Me.PictureBox18)
+        Me.Controls.Add(Me.PictureBox17)
+        Me.Controls.Add(Me.PictureBox15)
+        Me.Controls.Add(Me.PictureBox14)
+        Me.Controls.Add(Me.PictureBox13)
+        Me.Controls.Add(Me.PictureBox12)
+        Me.Controls.Add(Me.PictureBox11)
+        Me.Controls.Add(Me.PictureBox10)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Pacman Insta"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox13, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox14, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox15, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox17, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox18, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.redGhost, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.yellowGhost, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pinkGhost, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox16, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pacman, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents PictureBox9 As PictureBox
+    Friend WithEvents PictureBox10 As PictureBox
+    Friend WithEvents PictureBox11 As PictureBox
+    Friend WithEvents PictureBox12 As PictureBox
+    Friend WithEvents PictureBox13 As PictureBox
+    Friend WithEvents PictureBox14 As PictureBox
+    Friend WithEvents PictureBox15 As PictureBox
+    Friend WithEvents PictureBox17 As PictureBox
+    Friend WithEvents PictureBox18 As PictureBox
+    Friend WithEvents redGhost As PictureBox
+    Friend WithEvents yellowGhost As PictureBox
+    Friend WithEvents pinkGhost As PictureBox
+    Friend WithEvents PictureBox16 As PictureBox
+    Friend WithEvents Pacman As PictureBox
+End Class
---
+++ Pacman/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub PictureBox7_Click(sender As Object, e As EventArgs) Handles PictureBox7.Click
+
+    End Sub
+
+    Private Sub PictureBox16_Click(sender As Object, e As EventArgs) Handles redGhost.Click
+
+    End Sub
+End Class
